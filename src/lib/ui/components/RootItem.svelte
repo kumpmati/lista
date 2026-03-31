236cae9dfa04cbc953592a69f65e814f33cf8e69
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2026-03-31 16:11:47 +0300

    feat: add home page selection controls

---
+++ src/lib/ui/components/RootItem.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+	import { sleep } from '$lib/utils/sleep';
+	import { Share2 } from '@lucide/svelte';
+	import { Checkbox } from 'm3-svelte';
+	import { usePress } from 'svelte-gestures';
+	import { slide } from 'svelte/transition';
+
+	type Props = {
+		isSelectable?: boolean;
+		isSelected?: boolean;
+		id: string;
+		title: string;
+		items: number;
+		isPublic: boolean;
+		onLongPress?: () => void;
+		onToggleSelect?: (value: boolean) => void;
+	};
+
+	let { isSelectable, isSelected, id, title, items, isPublic, onLongPress, onToggleSelect }: Props =
+		$props();
+
+	const press = usePress(
+		() => !isSelectable && onLongPress?.(),
+		() => ({ timeframe: 300, triggerBeforeFinished: true }),
+		{
+			onpressdown: () => !isSelectable && (isLongPress = true),
+			onpressup: () => sleep(1).then(() => (isLongPress = false))
+		}
+	);
+
+	let selected = $derived(isSelectable && isSelected);
+
+	// to prevent instant deselection after long press,
+	// disable selection during long presses
+	let isLongPress = $state(false);
+
+	const handleClick = () => {
+		if (isLongPress) return; // don't toggle when releasing after long press
+
+		if (!isSelectable) {
+			goto(resolve('/(app)/list/[listId]', { listId: id }));
+			return;
+		}
+
+		onToggleSelect?.(!isSelected);
+	};
+</script>
+
+<button class="m3-layer" class:selected {...press} onclick={handleClick}>
+	{#if isSelectable}
+		<label transition:slide={{ axis: 'y', duration: 150 }}>
+			<Checkbox>
+				<input type="checkbox" checked={selected} tabindex="-1" />
+			</Checkbox>
+		</label>
+	{/if}
+
+	{#if isPublic}
+		<Share2 style="min-width: 16px; height: 16px; position: absolute; top: 1rem; right: 1rem;" />
+	{/if}
+
+	<span class="title">{title}</span>
+	<span class="items">{items || 0} items</span>
+</button>
+
+<style>
+	label {
+		user-select: none;
+		pointer-events: none;
+		margin-bottom: 0.5rem;
+	}
+
+	button {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		text-decoration: none;
+		background-color: transparent;
+		text-align: left;
+		width: 100%;
+
+		padding: 1rem;
+		border-radius: 0.75rem;
+		background-color: var(--m3c-surface-container);
+
+		transition: box-shadow 150ms;
+
+		&.selected {
+			box-shadow: 0 0 0 2px var(--m3c-primary);
+		}
+	}
+
+	.title {
+		font-size: 16px;
+		font-weight: bold;
+		color: var(--white);
+
+		width: calc(100% - 1rem);
+
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		overflow: hidden;
+	}
+
+	.items {
+		color: var(--m3c-primary);
+		margin-top: 0.25rem;
+		font-size: 14px;
+	}
+</style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,16 +1,56 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
+	import RootItem from '$lib/ui/components/RootItem.svelte';
+	import DeleteDialog from '$lib/ui/DeleteDialog.svelte';
 	import Header from '$lib/ui/layout/Header.svelte';
 	import Main from '$lib/ui/layout/Main.svelte';
-	import { Plus, Share2 } from '@lucide/svelte';
-	import { Button } from 'm3-svelte';
+	import { Plus, X } from '@lucide/svelte';
+	import { Button, Menu, MenuItem, SplitButton } from 'm3-svelte';
+	import { flip } from 'svelte/animate';
+	import { SvelteSet } from 'svelte/reactivity';
 
 	let { data } = $props();
+
+	let deleteDialogOpen = $state(false);
+	let isSelecting = $state(false);
+	const selected = new SvelteSet<string>();
 
 	const handleCreateList = async () => {
 		const item = await data.root.addList();
 		goto(resolve('/(app)/list/[listId]', { listId: item.id }));
+	};
+
+	const handleDeleteSelected = async () => {
+		await Promise.allSettled(selected.values().map((id) => data.root.removeList(id)));
+
+		handleStopSelect();
+	};
+
+	const handleStartSelect = (id: string) => {
+		isSelecting = true;
+		selected.clear();
+		selected.add(id);
+	};
+
+	const handleStopSelect = () => {
+		isSelecting = false;
+	};
+
+	const handleSetSelected = (id: string, value: boolean) => {
+		if (!value) {
+			selected.delete(id);
+		} else {
+			selected.add(id);
+		}
+	};
+
+	const handleSelectAll = () => {
+		data.root.current.items.forEach((item) => handleSetSelected(item.id, true));
+	};
+
+	const handleSelectNone = () => {
+		selected.clear();
 	};
 </script>
 
@@ -18,34 +58,68 @@
 	<title>Lista</title>
 </svelte:head>
 
+<DeleteDialog
+	bind:open={deleteDialogOpen}
+	headline="Confirm deletion"
+	onDelete={handleDeleteSelected}
+>
+	Permanently delete {selected.size} lists? This cannot be undone.
+</DeleteDialog>
+
 <Main>
 	<Header>
-		<h1>Lista</h1>
-		<Button
-			square
-			size="s"
-			variant="text"
-			iconType="full"
-			onclick={handleCreateList}
-			style="margin-right: .5rem; margin-left: auto;"
-		>
-			<Plus />
-		</Button>
+		{#if isSelecting}
+			<h1>{selected.size} selected</h1>
+
+			<div style="margin-left: auto;">
+				<SplitButton
+					onclick={handleStopSelect}
+					variant="tonal"
+					x="right"
+					y="down"
+					title="Stop selecting"
+				>
+					<X />
+
+					{#snippet menu()}
+						<Menu>
+							<MenuItem onclick={handleSelectAll}>Select all</MenuItem>
+							<MenuItem onclick={handleSelectNone}>Select none</MenuItem>
+							<MenuItem onclick={() => (deleteDialogOpen = true)} disabled={selected.size === 0}>
+								Delete selected
+							</MenuItem>
+						</Menu>
+					{/snippet}
+				</SplitButton>
+			</div>
+		{:else}
+			<h1>My lists</h1>
+
+			<Button
+				title="Create list"
+				variant="filled"
+				iconType="full"
+				onclick={handleCreateList}
+				style="margin-right: .5rem; margin-left: auto;"
+			>
+				<Plus />
+			</Button>
+		{/if}
 	</Header>
 
 	<ul class="list">
 		{#each data.root.current.items as item (item.id)}
-			<li>
-				<a class="list-item m3-layer" href={resolve('/(app)/list/[listId]', { listId: item.id })}>
-					{#if item.public}
-						<Share2
-							style="min-width: 16px; height: 16px; position: absolute; top: 1rem; right: 1rem;"
-						/>
-					{/if}
-
-					<span class="title">{item.title}</span>
-					<span class="items">{item.items || 0} items</span>
-				</a>
+			<li animate:flip={{ duration: 150 }}>
+				<RootItem
+					isSelectable={isSelecting}
+					isSelected={selected.has(item.id)}
+					id={item.id}
+					title={item.title}
+					items={item.items}
+					isPublic={item.public}
+					onLongPress={() => handleStartSelect(item.id)}
+					onToggleSelect={(val) => handleSetSelected(item.id, val)}
+				/>
 			</li>
 		{:else}
 			<li>No lists yet.</li>
@@ -69,33 +143,6 @@
 	}
 
 	li {
-		display: contents;
-	}
-
-	.list-item {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		border-radius: 0.75rem;
-		background-color: var(--m3c-surface-container);
-		text-decoration: none;
-
-		.title {
-			font-size: 16px;
-			font-weight: bold;
-			color: var(--white);
-
-			width: calc(100% - 1rem);
-
-			text-overflow: ellipsis;
-			white-space: nowrap;
-			overflow: hidden;
-		}
-
-		.items {
-			margin-top: 0.25rem;
-			font-size: 14px;
-		}
+		display: block;
 	}
 </style>
