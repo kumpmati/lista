e860fd57e644541bf7a7cd50b8b57b03fcd501e8
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2026-04-01 13:53:05 +0300

    chore(sync): update hostname

--- sync/src/utils/repo.ts
+++ sync/src/utils/repo.ts
@@ -1,12 +1,10 @@
-import { NetworkAdapter, Repo, RepoConfig } from '@automerge/automerge-repo';
-
-const hostname = 'TODO';
+import { NetworkAdapter, Repo } from '@automerge/automerge-repo';
 
 export const createRepo = (adapter: NetworkAdapter) => {
 	return new Repo({
 		network: [adapter],
 		/** @ts-ignore @type {(import("@automerge/automerge-repo").PeerId)}  */
-		peerId: `sync-server-${hostname}`,
+		peerId: `sync-server-lista.matsku.dev`,
 		// Since this is a server, we don't share generously — meaning we only sync documents they already
 		// know about and can ask for by ID.
 		sharePolicy: async () => false,
